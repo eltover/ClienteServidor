e09671f937977c0fe3ee3857e78ba5285e82c24c
David Tovar Martinez <dtovar@utp.edu.co>
2018-04-09 23:43:43 -0500

    graficas de las consultas turisticas

--- TriPereira/Destinos/views.py
+++ TriPereira/Destinos/views.py
@@ -7,9 +7,13 @@ from django.shortcuts import HttpResponse
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import plotly.offline as opy
+import plotly.graph_objs as go
 import twitter
 import json
+import pandas
 import pandas as pd
+import numpy as np
 from sodapy import Socrata
 
 # Create your views here.
@@ -27,18 +31,20 @@ def logout(request):
     return render_to_response('index.html', {}, RequestContext(request))
 
 def get_data(request):
-	template = 'destinos/data.html'
-	client = Socrata("www.datos.gov.co", None)
+    template = 'destinos/data.html'
+    client = Socrata("www.datos.gov.co", None)
 
-	results = client.get("87tt-fa3a", limit=2000)
-#print results
-	results_df = pd.DataFrame.from_records(results)
-    
-	context = {
-		'results_df': client.get("87tt-fa3a", limit=2000)
-	}
-	return render (request, template, context)
+    results = client.get("87tt-fa3a", limit=2000)
+    results_df = pd.DataFrame.from_records(results)
 
+      
+    context = {
+        'results_df': client.get("87tt-fa3a", limit=2000),
+        
+    }
+    return render (request, template, context)
+
+# CONSULTA DE DATOS
 
 def resultdata(request):
     value = request.POST.get('codigo')
@@ -46,15 +52,38 @@ def resultdata(request):
     client = Socrata("www.datos.gov.co", None)
 
     results = client.get("87tt-fa3a", limit=2000)
-#print results
+    #print results
     results_df = pd.DataFrame.from_records(results)
+
+    xresult=[]
+    yresult=[]
+    
+    #SACA CADA UNO DE LOS DATOS PARA LAS GRAFICAS
+    for t in range (len(results_df)):
+        if (True != pd.isnull (results_df.habitaciones[t])):
+            x=(results_df.habitaciones[t])
+            xresult.append(pd.to_numeric(x))
+        if (True != pd.isnull(results_df.tipo[t])):    
+            y=(results_df.tipo[t])
+            yresult.append(y)
+           
+    xresults=sorted(xresult)
+    yresults=yresult
+    
+
     context = {
-        'codigo': value,
+        'codigo': value,        
         'codigo2': value2,
-        'results_df': client.get("87tt-fa3a", limit=2000)
+        'results_df': client.get("87tt-fa3a", limit=2000),
+        'graph': pd.DataFrame.from_records(results),
+        'x': xresults,
+        'y': yresults        
     }
     template = 'destinos/resultdata.html'
     return render(request, template, context)
+
+
+#TWITTER
 
 @login_required
 def get_followers(request):
